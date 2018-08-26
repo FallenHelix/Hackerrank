9f3dfe75f344f3308ef4e6905f540912c76bd20a
Shubham Bhakuni <42718369+FallenHelix@users.noreply.github.com>
2018-08-26 21:32:45 +0530

    Update Conditional_Statements.cpp

--- Cpp/Introduction/Conditional_Statements.cpp
+++ Cpp/Introduction/Conditional_Statements.cpp
@@ -28,27 +28,18 @@ int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
     int N;
     cin >> N;
-    if (N == 1) {
-        cout << "one" << endl;
-    } else if (N == 2) {
-        cout << "two" << endl;
-    } else if (N == 3) {
-        cout << "three" << endl;
-    } else if (N == 4) {
-        cout << "four" << endl;
-    } else if (N == 5) {
-        cout << "five" << endl;
-    } else if (N == 6) {
-        cout << "six" << endl;
-    } else if (N == 7) {
-        cout << "seven" << endl;
-    } else if (N == 8) {
-        cout << "eight" << endl;
-    } else if (N == 9) {
-        cout << "nine" << endl;
-    } else {
-        cout << "Greater than 9" << endl;
+    char *array[11]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    if(n<=9)
+    {
+        for(int i=0;i<=n;i++)
+        {
+            if (i==n)
+                cout<<array[i];
+        }
+        
     }
+    else
+        cout<<"Greater than 9";
  
    return 0;
 }
